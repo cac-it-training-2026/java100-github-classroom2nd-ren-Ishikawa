5b6231c7873c89d5abfa12abe7ed19a9f06f636c
rishika <rishika@C010422930.zipangu.cac.co.jp>
2026-04-15 10:41:42 +0900

    2ndの俺の回答

--- Java100_questions_cac2nd/src/lesson01/challenge06/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge06/Patisserie.java
@@ -21,6 +21,47 @@
 
 package lesson01.challenge06;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Patisserie {
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("それぞれ何個ずつ買いますか？（最大30個まで）");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("シトロン      >");
+		String str1 = reader.readLine();
+		System.out.print("ショコラ      >");
+		String str2 = reader.readLine();
+		System.out.print("ピスターシュ  >");
+		String str3 = reader.readLine();
+
+		System.out.println("シトロン     " + str1 + "個");
+		System.out.println("ショコラ     " + str2 + "個");
+		System.out.println("ピスターシュ " + str3 + "個");
+
+		int inum1 = Integer.parseInt(str1);
+		int inum2 = Integer.parseInt(str2);
+		int inum3 = Integer.parseInt(str3);
+
+		System.out.println("合計個数    " + (inum1 + inum2 + inum3) + "個");
+		System.out.println("合計金額  " + (inum1 * 250 + inum2 * 280 + inum3 * 320) + "円");
+
+		System.out.println("をお買い上げですね。");
+		System.out.println("承りました。");
+
+		int a = 30 - inum1;
+		int b = 30 - inum2;
+		int c = 30 - inum3;
+
+		System.out.println("本日のおすすめ商品です。");
+		System.out.println("シトロン      \\250 ・・・ 残り" + a + "個");
+		System.out.println("ショコラ      \\280 ・・・ 残り" + b + "個");
+		System.out.println("ピスターシュ  \\320 ・・・ 残り" + c + "個");
+
+	}
 
 }
--- Java100_questions_cac2nd/src/lesson01/challenge07/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge07/Patisserie.java
@@ -41,6 +41,47 @@
 
 package lesson01.challenge07;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Patisserie {
 
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("それぞれ何個ずつ買いますか？（最大30個まで）");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("シトロン      >");
+		String str1 = reader.readLine();
+		System.out.print("ショコラ      >");
+		String str2 = reader.readLine();
+		System.out.print("ピスターシュ  >");
+		String str3 = reader.readLine();
+
+		System.out.println("シトロン     " + str1 + "個");
+		System.out.println("ショコラ     " + str2 + "個");
+		System.out.println("ピスターシュ " + str3 + "個");
+
+		double dnum1 = Double.parseDouble(str1);
+		double dnum2 = Double.parseDouble(str2);
+		double dnum3 = Double.parseDouble(str3);
+
+		System.out.println("合計個数    " + (dnum1 + dnum2 + dnum3) + "個");
+		System.out.println("合計金額  " + (int) (dnum1 * 250 + dnum2 * 280 + dnum3 * 320) + "円");
+
+		System.out.println("をお買い上げですね。");
+		System.out.println("承りました。");
+
+		int a = (int) (30 - dnum1);
+		int b = (int) (30 - dnum2);
+		int c = (int) (30 - dnum3);
+
+		System.out.println("本日のおすすめ商品です。");
+		System.out.println("シトロン      \\250 ・・・ 残り" + a + "個");
+		System.out.println("ショコラ      \\280 ・・・ 残り" + b + "個");
+		System.out.println("ピスターシュ  \\320 ・・・ 残り" + c + "個");
+
+	}
 }
